3dca5ee7f1b0d846e420f16811626d9c56e5c537
Alex <3707177+skipidar@users.noreply.github.com>
2024-03-16 18:39:06 +0100

    Fixing the provider and switching to linux function

--- infrastructure/terraform/project_az_function/environments/env1/resource_azure_function.tf
+++ infrastructure/terraform/project_az_function/environments/env1/resource_azure_function.tf
@@ -13,20 +13,23 @@ resource "azurerm_service_plan" "sp" {
 #  sku_name            = "B1" # Basic small dedicated plan, scalable
 }
 
-resource "azurerm_application_insights" "example" {
+resource "azurerm_application_insights" "az_func_insights" {
   name                = "${var.prefix}-appinsights"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   application_type    = "web"
 }
 
-resource "azurerm_linux_function_app" "example" {
+resource "azurerm_linux_function_app" "az_func_app" {
   name                       = local.az_function_name
   location                   = local.location
   resource_group_name        = azurerm_resource_group.rg.name
+
   service_plan_id             = azurerm_service_plan.sp.id
   storage_account_name       = azurerm_storage_account.storage_account.name
   storage_account_access_key = azurerm_storage_account.storage_account.primary_access_key
+  https_only                 = true
+  public_network_access_enabled  = true
 
   identity {
     type = "SystemAssigned"
@@ -34,7 +37,7 @@ resource "azurerm_linux_function_app" "example" {
 
   site_config {
     application_stack {
-      python_version = "3.9"
+      python_version = "3.11"
     }
   }
 }
